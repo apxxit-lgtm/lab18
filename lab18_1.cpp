c7b6ecde7bc22ad5ff1afce942e80c5f9f3911a2
apxxit-lgtm <apxxit@gmail.com>
2026-02-11 23:35:05 +0700

    lab18_3

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -8,23 +8,22 @@ struct Rect {
 };
 
 double overlap(Rect R1, Rect R2) {
-    // คำนวณขอบของ R1 (ใช้ x, y เป็นจุดศูนย์กลาง)
+
     double R1_left   = R1.x - R1.w / 2.0;
     double R1_right  = R1.x + R1.w / 2.0;
     double R1_bottom = R1.y - R1.h / 2.0;
     double R1_top    = R1.y + R1.h / 2.0;
 
-    // คำนวณขอบของ R2
-    double R2_left   = R2.x - R2.w / 2.0;
+        double R2_left   = R2.x - R2.w / 2.0;
     double R2_right  = R2.x + R2.w / 2.0;
     double R2_bottom = R2.y - R2.h / 2.0;
     double R2_top    = R2.y + R2.h / 2.0;
 
-    // หาช่วงที่ทับซ้อนกัน
+
     double overlap_w = min(R1_right, R2_right) - max(R1_left, R2_left);
     double overlap_h = min(R1_top, R2_top) - max(R1_bottom, R2_bottom);
 
-    // ตรวจสอบว่าทับกันจริงไหม
+
     if (overlap_w <= 0 || overlap_h <= 0) return 0;
 
     return overlap_w * overlap_h;
