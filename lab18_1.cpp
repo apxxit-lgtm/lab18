207e3474b9c137b9fb218e74af92608115dbb8a0
apxxit-lgtm <apxxit@gmail.com>
2026-02-13 14:45:15 +0700

    lab18_4

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -8,23 +8,33 @@ struct Rect {
 };
 
 double overlap(Rect R1, Rect R2) {
+    // หาขอบของสี่เหลี่ยมรูปที่ 1
+    double L1 = R1.x;
+    double R1_right = R1.x + R1.w;
+    double T1 = R1.y;
+    double B1 = R1.y - R1.h; // ปกติพิกัด y มุมซ้ายบน ค่า y จะลดลงเมื่อลงข้างล่าง
 
-    double R1_left   = R1.x - R1.w / 2.0;
-    double R1_right  = R1.x + R1.w / 2.0;
-    double R1_bottom = R1.y - R1.h / 2.0;
-    double R1_top    = R1.y + R1.h / 2.0;
+    // หาขอบของสี่เหลี่ยมรูปที่ 2
+    double L2 = R2.x;
+    double R2_right = R2.x + R2.w;
+    double T2 = R2.y;
+    double B2 = R2.y - R2.h;
 
-        double R2_left   = R2.x - R2.w / 2.0;
-    double R2_right  = R2.x + R2.w / 2.0;
-    double R2_bottom = R2.y - R2.h / 2.0;
-    double R2_top    = R2.y + R2.h / 2.0;
+    // หาขอบของพื้นที่ส่วนที่ทับกัน
+    double overlap_L = max(L1, L2);
+    double overlap_R = min(R1_right, R2_right);
+    double overlap_T = min(T1, T2);
+    double overlap_B = max(B1, B2);
 
+    // คำนวณความกว้างและความสูงของส่วนที่ทับกัน
+    double overlap_w = overlap_R - overlap_L;
+    double overlap_h = overlap_T - overlap_B;
 
-    double overlap_w = min(R1_right, R2_right) - max(R1_left, R2_left);
-    double overlap_h = min(R1_top, R2_top) - max(R1_bottom, R2_bottom);
-
-
-    if (overlap_w <= 0 || overlap_h <= 0) return 0;
+    // ถ้าค่าใดค่าหนึ่งเป็นลบหรือศูนย์ แสดงว่าไม่ทับกัน
+    if (overlap_w <= 0 || overlap_h <= 0) {
+        return 0;
+    }
 
     return overlap_w * overlap_h;
 }
+
