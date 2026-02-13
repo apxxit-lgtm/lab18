acf3000f01eb56f1098ba2c5b2353800e96dc9bc
apxxit-lgtm <apxxit@gmail.com>
2026-02-13 14:48:25 +0700

    lab18_4

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -8,29 +8,26 @@ struct Rect {
 };
 
 double overlap(Rect R1, Rect R2) {
-    // หาขอบของสี่เหลี่ยมรูปที่ 1
+    
     double L1 = R1.x;
     double R1_right = R1.x + R1.w;
     double T1 = R1.y;
-    double B1 = R1.y - R1.h; // ปกติพิกัด y มุมซ้ายบน ค่า y จะลดลงเมื่อลงข้างล่าง
+    double B1 = R1.y - R1.h;
 
-    // หาขอบของสี่เหลี่ยมรูปที่ 2
+
     double L2 = R2.x;
     double R2_right = R2.x + R2.w;
     double T2 = R2.y;
     double B2 = R2.y - R2.h;
 
-    // หาขอบของพื้นที่ส่วนที่ทับกัน
     double overlap_L = max(L1, L2);
     double overlap_R = min(R1_right, R2_right);
     double overlap_T = min(T1, T2);
     double overlap_B = max(B1, B2);
 
-    // คำนวณความกว้างและความสูงของส่วนที่ทับกัน
     double overlap_w = overlap_R - overlap_L;
     double overlap_h = overlap_T - overlap_B;
 
-    // ถ้าค่าใดค่าหนึ่งเป็นลบหรือศูนย์ แสดงว่าไม่ทับกัน
     if (overlap_w <= 0 || overlap_h <= 0) {
         return 0;
     }
